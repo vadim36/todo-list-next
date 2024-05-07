ace517fc1c1a7a04ff540d2693bfc95ad04d9ece
Vadim Medvedev <vadim130809@gmail.com>
2024-05-07 18:43:42 +0300

    add validation

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import CreateUserDto from './dto/create-user-dto';
 import { UsersService } from './users.service';
@@ -11,6 +11,7 @@ export class UsersController {
   
   @ApiOperation({summary: 'Create a new user'})
   @ApiResponse({status: 201, type: UserModel})
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() userDto: CreateUserDto) {
     return await this.userService.createUser(userDto)
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/db/db.service';
 import CreateUserDto from './dto/create-user-dto';
 import UserModel from 'src/models/user.model';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
   
   async createUser(userDto: CreateUserDto):Promise<UserModel> {
+    const candidate = await this.prismaService.user.findFirst({
+      where: { OR: [
+        { email: userDto.email },
+        { refreshToken: userDto.refreshToken }
+      ]},
+      include: {tasks: { include: { user: true }}}
+    })
+
+    if (candidate !== null) {
+      throw new HttpException('Such user already exist', HttpStatus.BAD_REQUEST)
+    }
+
     return await this.prismaService.user.create({
       data: {...userDto},
       include: {tasks: { include: { user: true }}}
