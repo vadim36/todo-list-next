5715922bc3eec2ddda709d52976c405a1fa9d0ea
Vadim Medvedev <vadim130809@gmail.com>
2024-05-08 15:20:02 +0300

    add db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config'
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env'
-    })
+    }),
+    DbModule
   ]
 })
 export default class AppModule {}
