f9895682e574b20bfaf9882b3f58d20817bf1e9c
Vadim Medvedev <vadim130809@gmail.com>
2024-05-15 10:55:48 +0300

    add pages

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import { LoginPage } from "@/pages/Login";
+
+export default function Login() {
+  return <LoginPage/>
+}
---
+++ client/src/pages/SignUp/components/SignUpPage.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export function SignUpPage() {
+  return (
+    <>
+     <h1>SignUp Page</h1>
+     <Link href="/login">Log in</Link>
+    </>
+  )
+}
